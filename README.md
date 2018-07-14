a2f673e3a7fa7946469dc8a136762ebc97a1cbbc
esme518 <esme518@gmail.com>
2018-07-14 23:56:10 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+:whale: Dockerfiles for [V2Ray](https://github.com/v2ray/v2ray-core)
+
+forked from v2ray/install
---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -f UUID ]; then
+	cat /proc/sys/kernel/random/uuid > UUID
+fi
+
+UUID=$(cat UUID)
+
+# Set config.json
+sed -i "s/PORT/$PORT/g" /etc/v2ray/config.json
+sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+sed -i "s/WSPATH/$WSPATH/g" /etc/v2ray/config.json
+
+echo starting v2ray platform
+echo starting with UUID:$UUID
+echo listening at 0.0.0.0:$PORT$WSPATH
+
+exec "$@"
